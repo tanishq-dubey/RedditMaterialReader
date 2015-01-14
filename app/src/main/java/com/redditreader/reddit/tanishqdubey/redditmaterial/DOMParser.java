1cb4293748f2ff3a855e4a8c37f6ea5246248f4a
Tanishq Dubey <tanishq.dubey@gmail.com>
2015-01-14 12:53:20 -0600

    Finished DOMParser

--- app/src/main/java/com/redditreader/reddit/tanishqdubey/redditmaterial/DOMParser.java
+++ app/src/main/java/com/redditreader/reddit/tanishqdubey/redditmaterial/DOMParser.java
@@ -4,16 +4,17 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
-/**
- * Created by Tanishq Dubey on 1/14/2015.
- */
+
 public class DOMParser {
 
     private RSSFeed _feed = new RSSFeed();
@@ -44,8 +45,39 @@ public class DOMParser {
 
                 NodeList nodeChild = currentNode.getChildNodes();
                 int childNodeLength = nodeChild.getLength();
+
+                for (int j = 0; j < childNodeLength; j = j + 1){
+                    Node thisNode = nodeChild.item(j);
+                    String theString = null;
+                    String nodeName = thisNode.getNodeName();
+
+                    theString = nodeChild.item(j).getFirstChild().getNodeValue();
+
+                    if (theString != null){
+                        if ("title".equals(nodeName)){
+                            _item.set_title(theString);
+                        } else if ("description".equals(nodeName)){
+                            _item.set_description(theString);
+                        } else if ("pubDate".equals(nodeName)){
+                            String formattedDate = theString.replace(" +0000", "");
+                            _item.set_date(formattedDate);
+                        }
+                    }
+                }
+                _feed.addItem(_item);
             }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (NullPointerException e){
+            e.printStackTrace();
         }
+
+        return _feed;
+
     }
 
 }
